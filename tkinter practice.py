633c9da8ac17abb76cacc41727da6f7d31ac726f
richard523 <rgatchalian523@gmail.com>
2021-09-07 15:05:35 -0500

    sentimentAnalysisSolFinal.ipynb
    only need to pass through bert
    and attempt logical negation

---
+++ tkinter practice.py
@@ -0,0 +1,87 @@
+import sys
+from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication,
+    QVBoxLayout, QDialog)
+
+class Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        # Create widgets
+        self.edit = QLineEdit("Write my name here")
+        self.button = QPushButton("Show Greetings")
+        self.newButton = QPushButton("HELLONEWBUTTON")
+        # Create layout and add widgets
+        layout = QVBoxLayout()
+        layout.addWidget(self.edit)
+        layout.addWidget(self.button)
+        layout.addWidget(self.newButton)
+        # Set dialog layout
+        self.setLayout(layout)
+        # Add button signal to greetings slot
+        self.button.clicked.connect(self.greetings)
+        # Add another button
+        
+    # Greets the user
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
+
+if __name__ == '__main__':
+    # Create the Qt Application
+    app = QApplication(sys.argv)
+    # Create and show the form
+    form = Form()
+    form.show()
+    # Run the main Qt loop
+    sys.exit(app.exec())
+
+
+
+
+"""
+import sys
+from PySide6.QtGui import QColor
+from PySide6.QtWidgets import (QApplication, QTableWidget,
+                               QTableWidgetItem)
+
+
+
+colors = [("Red", "#FF0000"),
+          ("Green", "#00FF00"),
+          ("Blue", "#0000FF"),
+          ("Black", "#000000"),
+          ("White", "#FFFFFF"),
+          ("Electric Green", "#41CD52"),
+          ("Dark Blue", "#222840"),
+          ("Yellow", "#F9E56d")]
+
+
+
+def get_rgb_from_hex(code):
+    code_hex = code.replace("#", "")
+    rgb = tuple(int(code_hex[i:i+2], 16) for i in (0, 2, 4))
+    return QColor.fromRgb(rgb[0], rgb[1], rgb[2])
+
+
+
+app = QApplication()
+
+
+table = QTableWidget()
+table.setRowCount(len(colors))
+table.setColumnCount(len(colors[0]) + 1)
+table.setHorizontalHeaderLabels(["Name", "Hex Code", "Color"])
+
+
+for i, (name, code) in enumerate(colors):
+    item_name = QTableWidgetItem(name)
+    item_code = QTableWidgetItem(code)
+    item_color = QTableWidgetItem()
+    item_color.setBackground(get_rgb_from_hex(code))
+    table.setItem(i, 0, item_name)
+    table.setItem(i, 1, item_code)
+    table.setItem(i, 2, item_color)
+
+
+table.show()
+sys.exit(app.exec())
+"""
